f9f96a0402c6c9ab0163e44b7cf11d524c370b2e
Kevin Juan Jacque Castillo <kevinjuanjacquecastillo@MacBook-de-Kevin.local>
2020-08-20 01:27:34 -0400

    agregando public

--- servicio/index.js
+++ servicio/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const morgan= require('morgan');
 const router = require('./router/router');
 const app=express();
+const path=require('path')
 
 
 app.set('port',process.env.PORT || 4000);
@@ -12,6 +13,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(router);
+
+app.use(express.static(path.resolve(__dirname,'../public/')))
 
 app.listen(app.get('port'),()=>{
     console.log('server on port ', app.get('port'));
